92f4361c4f5e1bafcd903336cf3258db49030612
Christian Dilger <c.dilger@addorange.de>
2021-03-29 13:23:36 +0200

    [CHANGE] allow float values on benchmarkValues

--- ext_tables.sql
+++ ext_tables.sql
@@ -81,7 +81,7 @@ CREATE TABLE tx_rkwsurvey_domain_model_question (
 	required int(11) DEFAULT '0' NOT NULL,
 	group_by int(11) DEFAULT '0' NOT NULL,
 	benchmark int(11) DEFAULT '0' NOT NULL,
-	benchmark_value int(11) DEFAULT '0' NOT NULL,
+	benchmark_value double DEFAULT '0' NOT NULL,
 	answer_option text NOT NULL,
 	survey int(11) unsigned DEFAULT '0',
 	topic int(11) unsigned DEFAULT '0' NOT NULL,
