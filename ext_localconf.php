aa7c884356674941371babc44cd9b6a6ef1609f6
Christian Dilger <c.dilger@addorange.de>
2024-02-13 09:46:56 +0100

    [FIX] deactivate XClass stuff due to problems in TYPO3 9.5

--- ext_localconf.php
+++ ext_localconf.php
@@ -21,19 +21,20 @@ call_user_func(
 		);
 
         //=================================================================
+        // ATTENTION: deactivated due to faulty mapping in TYPO3 9.5
         // Add XClasses for extending existing classes
         //=================================================================
-        // for TYPO3 12+
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\Madj2k\FeRegister\Domain\Model\BackendUser::class] = [
-            'className' => \RKW\RkwSurvey\Domain\Model\BackendUser::class
-        ];
-
-        // for TYPO3 9.5 - 11.5 only, not required for TYPO3 12
-        \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\Container\Container::class)
-            ->registerImplementation(
-                \Madj2k\FeRegister\Domain\Model\BackendUser::class,
-                \RKW\RkwSurvey\Domain\Model\BackendUser::class
-            );
+//        // for TYPO3 12+
+//        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\Madj2k\FeRegister\Domain\Model\BackendUser::class] = [
+//            'className' => \RKW\RkwSurvey\Domain\Model\BackendUser::class
+//        ];
+//
+//        // for TYPO3 9.5 - 11.5 only, not required for TYPO3 12
+//        \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\Container\Container::class)
+//            ->registerImplementation(
+//                \Madj2k\FeRegister\Domain\Model\BackendUser::class,
+//                \RKW\RkwSurvey\Domain\Model\BackendUser::class
+//            );
 
         //=================================================================
         // Setting Logger
